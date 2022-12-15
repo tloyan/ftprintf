1398351da63c2ceed9ebc9c10f006da1e2f2b08e
thomas loyan <thomas.loyan@gmail.com>
2022-12-15 17:21:00 +0100

    add libft to repository

---
+++ libft/includes/libft.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thloyan <thloyan@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/01 14:37:47 by thloyan           #+#    #+#             */
+/*   Updated: 2022/12/02 20:13:49 by thloyan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <limits.h>
+# include <string.h>
+# include	<stdlib.h>
+# include	<unistd.h>
+
+# include "libc/ft_libc_ctype.h"
+# include "libc/ft_libc_stdlib.h"
+# include "libc/ft_libc_string.h"
+# include "libc/ft_libc_strings.h"
+
+# include "string/ft_string.h"
+# include "int/ft_int.h"
+# include "io/ft_io.h"
+# include "list/ft_list.h"
+
+#endif
